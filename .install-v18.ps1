250713c7807062c59f879d67909224b3f200cac8
unknown <Eleme@DESKTOP-7LBO366.ad.intra.regen-bio.com>
2026-09-02 16:48:42 +0800

    chore: drop temporary installer helper

--- .install-v18.ps1
+++
@@ -1,4 +0,0 @@
-$msi = 'C:\Users\Eleme\codex_workspace\overseas-access-gateway\.worktrees\windows-forwarding-poc\dist\OverseasAccessSetup-v18-RELEASE_SIGNED.msi'
-$log = 'C:\Users\Eleme\codex_workspace\overseas-access-gateway\.worktrees\windows-forwarding-poc\build\v18-install.log'
-$p = Start-Process -FilePath msiexec.exe -ArgumentList @('/i', $msi, '/qn', '/l*v', $log) -Wait -PassThru
-Write-Output ("msiexec exit: " + $p.ExitCode)
